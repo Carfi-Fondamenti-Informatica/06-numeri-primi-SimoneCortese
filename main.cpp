7680a53d97db8c2c80b0f0a23b501299262cef97
SimoneCortese <117988407+SimoneCortese@users.noreply.github.com>
2022-12-02 13:56:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
+#include "lib.h"
 int main(){
+  int h;
+  std::cin>>h;
+  if (numeroprimo(h) == 0){
+  std::cout<<"numero primo" <<
+    std::endl;
+  }
+  else{
+  std::cout<<"numero non primo" <<
+    std::endl;
+  }
   return 0;
 }

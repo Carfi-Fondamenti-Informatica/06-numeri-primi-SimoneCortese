6ee2302d7d2e07ce681ccd167d504a9cb5978451
SimoneCortese <117988407+SimoneCortese@users.noreply.github.com>
2022-12-02 14:17:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@ int main(){
   int h;
   std::cin>>h;
   if (numeroprimo(h) == 0){
-  std::cout<<"numero primo" << std::endl;
+  std::cout<<"numero primo"<<
+    std::endl;
   }
   else{
-  std::cout<<"numero non primo" << std::endl;
+  std::cout<<"numero non primo"<<
+    std::endl;
   }
   return 0;
 }
